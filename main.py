b09c06800dbda948da3bb4d9886e5c5f830c6bdb
tomy1230 <122602344+tomy1230@users.noreply.github.com>
2023-04-07 16:06:05 +0300

    Update main.py
    add comments

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import requests
 
 def First_name():
     response = requests.get('https://randomuser.me/api/').json()
-    first_name = response['results'][0]['name']['first']
+    first_name = response['results'][0]['name']['first']  #get random first name from first user spot in the dict
     return first_name
 
 
 def User_value_run(number1):
     if 0 < number <= 100:
-        for num in range(number1):
-            ran_user = First_name()
+        for num in range(number1):  #for the range of the user input
+            ran_user = First_name()  # use First_name class in order to get the first name 
             print(ran_user)
-    elif number == -1:
+    elif number == -1:   
         print('You quit!')
     else:
         print("Enter number greater then 0!!!")
 
 
 number=1
-while number != -1:
-    number = int(input('Enter number between 1 - 100(enter -1 to quit):\n'))
-    User_value_run(number)
+while number != -1: # until the user enters '-1' 
+    number = int(input('Enter number between 1 - 100(enter -1 to quit):\n'))  #user input
+    User_value_run(number) # send number to the class
